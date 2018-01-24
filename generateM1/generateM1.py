d8d9c63cc4d3199877b8d3e45db8faee58d3456d
Matty Jones <mattyjones@boisestate.edu>
2018-01-24 16:48:03 -0700

    Completed the M1 generation script. Had to add a new atom in, fix some old bonds and add new ones

--- generateM1/generateM1.py
+++ generateM1/generateM1.py
@@ -30,10 +30,57 @@ class m1_surface(mb.Compound):
     def __init__(self, surface_dimensions=[1, 1], stoichiometry_dict={'Mo': 1, 'V': 0.2, 'Nb': 0.17}):
         # Call the mb.Compound initialisation
         super().__init__()
-        current_cell = m1_unit_cell(stoichiometry_dict)
-        self.add(current_cell)
+        # Specify some dimensions for the templateM1.pdb file that we're using (these are esimated from the atom positions
+        # in the template)
+        x_extent = 2.1189  # mbuild assumes nm
+        y_extent = 2.6738  # mbuild assumes nm
+        # OUTER LOOP: Multiply up each x_row to create as many y repeats as specified
+        previous_row = None
+        complete_cell_matrix = []  # This is required for new bonds across diagonal elements
+        for y_repeat in range(surface_dimensions[1]):
+            current_row = []
+            # INNER LOOP: First, create as many x repeats as specified
+            # Note: Each cell has 159 atoms in it
+            previous_cell = None
+            for x_repeat in range(surface_dimensions[0]):
+                current_cell = m1_unit_cell(stoichiometry_dict)
+                current_row.append(current_cell)
+                current_cell.translate([x_repeat * x_extent, y_repeat * y_extent, 0])
+                self.add(current_cell)
+                if previous_cell is not None:
+                    self.add_bond([previous_cell[60], current_cell[21]])
+                    self.add_bond([previous_cell[137], current_cell[13]])
+                    self.add_bond([previous_cell[134], current_cell[16]])
+                    self.add_bond([previous_cell[122], current_cell[16]])
+                    self.add_bond([previous_cell[19], current_cell[119]])
+                    self.add_bond([previous_cell[66], current_cell[33]])
+                    self.add_bond([previous_cell[18], current_cell[65]])
+                previous_cell = current_cell
+            if previous_row is not None:
+                for cell_ID, current_cell_y in enumerate(current_row):
+                    previous_cell_y = previous_row[cell_ID]
+                    self.add_bond([previous_cell_y[72], current_cell_y[27]])
+                    self.add_bond([previous_cell_y[1], current_cell_y[58]])
+                    self.add_bond([previous_cell_y[1], current_cell_y[73]])
+                    self.add_bond([previous_cell_y[4], current_cell_y[123]])
+                    self.add_bond([previous_cell_y[4], current_cell_y[141]])
+                    self.add_bond([previous_cell_y[6], current_cell_y[141]])
+                    self.add_bond([previous_cell_y[6], current_cell_y[156]])
+                    self.add_bond([previous_cell_y[114], current_cell_y[12]])
+                    self.add_bond([previous_cell_y[159], current_cell_y[12]])
+            complete_cell_matrix.append(current_row)
+            previous_row = current_row
+        # Now that the cell_matrix is complete, there might be a few more bonds to add in
+        # Across the diagonals (i.e. [0, 0] bonded to [1, 1]; [0, 1] bonded to [1, 2] etc.)
+        # Go across all rows first
+        for y_coord in range(surface_dimensions[1]):
+            for x_coord in range(surface_dimensions[0]):
+                if (x_coord + 1 < surface_dimensions[0]) and (y_coord + 1 < surface_dimensions[1]):
+                    first_cell = complete_cell_matrix[x_coord][y_coord]
+                    second_cell = complete_cell_matrix[x_coord + 1][y_coord + 1]
+                    self.add_bond([first_cell[61], second_cell[21]])
 
 
 if __name__ == "__main__":
-    m1Crystal = m1_surface()
+    m1Crystal = m1_surface(surface_dimensions=[2, 2])
     m1Crystal.save('test.hoomdxml', overwrite=True)
