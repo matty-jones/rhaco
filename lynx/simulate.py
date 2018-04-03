193e62ea143a79aa3b198da8f1f63579e910dddd
Matty Jones <mattyjones@boisestate.edu>
2018-04-03 17:04:51 -0600

    Added in the reading of FF coeffs from the input XML, and added the temperature conversion

--- lynx/simulate.py
+++ lynx/simulate.py
@@ -4,45 +4,75 @@ import hoomd
 import hoomd.md
 import hoomd.deprecated
 import xml.etree.cElementTree as ET
+import numpy as np
 
+AVOGADRO = 6.022140857E23
+BOLTZMANN = 1.38064852E-23
+J_KCAL = 4.184E-3
 
-def set_coeffs(file_name):
+def set_coeffs(file_name, system):
     '''
     Read in the molecular dynamics coefficients exported by Foyer
     '''
-    get_coeffs(file_name)
+    coeffs_dict = get_coeffs(file_name)
     # Perylene: Sigma = 3.8 Ang, Epsilon = 0.1217 kCal/mol
     ljnl = hoomd.md.nlist.cell()
     lj = hoomd.md.pair.lj(r_cut=2.5, nlist=ljnl)
-    lj.pair_coeff.set('CP', 'CP', epsilon=1.0, sigma=1.0)
+    for type1 in coeffs_dict['pair_coeffs']:
+        for type2 in coeffs_dict['pair_coeffs']:
+            lj.pair_coeff.set(type1[0], type2[0],
+                              epsilon=np.sqrt(type1[1] * type2[1]),
+                              sigma=np.sqrt(type1[2] * type2[2]))
 
+    harmonic_bond = hoomd.md.bond.harmonic()
+    for bond in coeffs_dict['bond_coeffs']:
+        harmonic_bond.bond_coeff.set(bond[0], k=bond[1], r0=bond[2])
+
+    harmonic_angle = hoomd.md.angle.harmonic()
+    for angle in coeffs_dict['angle_coeffs']:
+        harmonic_angle.angle_coeff.set(angle[0], k=angle[1], t0=angle[2])
+
+    harmonic_dihedral = hoomd.md.dihedral.opls()
+    for dihedral in coeffs_dict['dihedral_coeffs']:
+        harmonic_dihedral.dihedral_coeff.set(dihedral[0], k1=dihedral[1],
+                                             k2=dihedral[2], k3=dihedral[3],
+                                             k4=dihedral[4])
+
+    for atomID, atom in enumerate(system.particles):
+        atom.mass = coeffs_dict['mass'][atomID]
+
+    # TODO: Support for charges
     #pppmnl = hoomd.md.nlist.cell()
     #pppm = hoomd.md.charge.pppm(group=hoomd.group.charged(), nlist = pppmnl)
     #pppm.set_params(Nx=64,Ny=64,Nz=64,order=6,rcut=2.70)
 
-    harmonic_bond = hoomd.md.bond.harmonic()
-    harmonic_bond.bond_coeff.set('CP-CP', k=30000.0, r0=0.4)
+    return system
 
-    #harmonic_angle = hoomd.md.angle.harmonic()
-    #harmonic_angle.angle_coeff.set('CP-CP-CP', k=380.0, t0=2.09)
-
-    #harmonic_dihedral = hoomd.md.dihedral.opls()
-    #harmonic_dihedral.dihedral_coeff.set('CP-CP-CP-CP', k1=0.0, k2=50.0, k3=0.0, k4=0.0)
 
 
 def get_coeffs(file_name):
-    coeff_dictionary = {}
-    constraint_types = ['pair', 'bond', 'angle', 'dihedral']
+    coeff_dictionary = {'pair_coeffs': [], 'bond_coeffs': [],
+                        'angle_coeffs': [], 'dihedral_coeffs': []}
     with open(file_name, 'r') as xml_file:
         xml_data = ET.parse(xml_file)
-    for constraint_type in constraint_types:
-        data = xml_data.find(constraint_type + '_coeffs').text
-        print(data)
-        exit()
-
-    #atomDictionary[key] = [list(map(int, x.split(' '))) for x in morphologyConfig.find(key).text.split('\n')[1:-1]]
-
-
+    root = xml_data.getroot()
+    for config in root:
+        for child in config:
+            # First get the masses which are different
+            if child.tag == 'mass':
+                coeff_dictionary['mass'] = [float(mass) for mass in
+                                            child.text.split('\n') if
+                                            len(mass) > 0]
+            # Now the other coefficients
+            elif child.tag in coeff_dictionary.keys():
+                for line in child.text.split('\n'):
+                    if len(line) == 0:
+                        continue
+                    coeff = line.split()
+                    coeff_dictionary[child.tag].append(
+                        [coeff[0]] + list(map(float, coeff[1:])))
+                # coeff_dictionary[child.tag] = child.text.split('\n')
+    return coeff_dictionary
 
 
 def initialize_velocities(snapshot, temperature):
@@ -66,48 +96,63 @@ def main():
                         type=float,
                         default=290,
                         required=False,
-                        help='''The desired temperature of the simulation in kelvin.\n
-                        If unspecified, the temperature is set to the default value of
-                        290 K.
-                        ''')
+                        help='''The desired temperature of the simulation in
+                        kelvin (this will be rescaled to produce a reduced
+                        temperature that conforms with Foyer's default
+                        units (kcal/mol and angstroems).\n
+                        If unspecified, the temperature is set to the default
+                        value of 290 K.''')
     parser.add_argument('-r', '--run_time',
                         type=float,
                         default=1e7,
                         required=False,
-                        help='''The number of timesteps to run the MD simulation for.\n
-                        If unspecified, the default runtime of 1e7 will be used.
-                        ''')
+                        help='''The number of timesteps to run the MD
+                        simulation for.\n
+                        If unspecified, the default runtime of 1e7 will be
+                        used.''')
     args, file_list = parser.parse_known_args()
+
+    # Foyer gives parameters in terms of kcal/mol for energies and angstroems
+    # for distances. Convert these to reduced units for HOOMD using the
+    # following conversion, and print a string to inform the user it has been
+    # done.
+    reduced_temperature = args.temperature * BOLTZMANN * J_KCAL * AVOGADRO
+    print("Using the Foyer default units of <DISTANCE> = 1 Angstroem and"
+          " <ENERGY> = 1 kcal/mol, the input temperature of ",
+          args.temperature, "corresponds to", reduced_temperature, "in"
+          " dimensionless HOOMD kT units.")
 
     for file_name in file_list:
         hoomd.context.initialize("")
         system = hoomd.deprecated.init.read_xml(filename=file_name)
-        # Set the correct atom Masses
-        for atom in system.particles:
-            atom.mass = masses[atom.type]
-
         snapshot = system.take_snapshot()
         # Assign the required velocities based on the requested temperature
-        initialized_snapshot = initialize_velocities(snapshot, args.temperature)
+        initialized_snapshot = something_else(snapshot,
+                                              reduced_temperature)
         # Finally, restore the snapshot
         system.restore_snapshot(initialized_snapshot)
-
-        set_coeffs(file_name)
+        system = set_coeffs(file_name, system)
 
         hoomd.md.integrate.mode_standard(dt=0.001);
-        carbons = hoomd.group.type('C')
-        hydrogens = hoomd.group.type('H')
-        hydrocarbons = hoomd.group.union(carbons, hydrogens)
-        integrator = hoomd.md.integrate.nvt(group=hydrocarbons, tau=0.1, kT=args.temperature)
+        carbons = hoomd.group.type(name='carbons', type='C')
+        hydrogens = hoomd.group.type(name='hydrogens', type='H')
+        hydrocarbons = hoomd.group.union(name='hydrocarbons', a=carbons,
+                                         b=hydrogens)
+        integrator = hoomd.md.integrate.nvt(group=hydrocarbons, tau=0.1,
+                                            kT=reduced_temperature)
         #hoomd.md.nlist.reset_exclusions(exclusions = ['bond', 'angle', 'dihedral', 'body'])
 
-        hoomd.dump.gsd(filename=".".join(file_name.split(".")[:-1]) + "_traj.gsd", period=int(args.run_time/500),
-                       overwrite=True)
-        logQuantities = ['temperature', 'pressure', 'volume', 'potential_energy', 'kinetic_energy',
-                         'pair_lj_energy', 'pair_ewald_energy', 'pppm_energy', 'bond_harmonic_energy',
-                         'angle_harmonic_energy', 'dihedral_opls_energy']
-        hoomd.analyze.log(filename=fileName.split('.')[0] + ".log", quantities=logQuantities,
-                            period=int(args.run_time/10000), header_prefix='#', overwrite=True)
+        hoomd.dump.gsd(filename=".".join(file_name.split(".")[:-1])
+                       + "_traj.gsd", period=max([int(args.run_time/500), 1]),
+                       group=hoomd.group.all(), overwrite=True)
+        log_quantities = ['temperature', 'pressure', 'volume',
+                          'potential_energy', 'kinetic_energy',
+                          'pair_lj_energy', 'bond_harmonic_energy',
+                          'angle_harmonic_energy', 'dihedral_opls_energy']
+        hoomd.analyze.log(filename='.'.join(file_name.split('.')[:-1])
+                          + ".log", quantities=log_quantities,
+                          period=max([int(args.run_time/10000), 1]),
+                          header_prefix='#', overwrite=True)
         ## Now incrementally ramp the charges
         #for chargePhase in range(chargeIncrements + 1):
         #    print("Incrementing charge phase", chargePhase, "of", chargeIncrements + 1)
