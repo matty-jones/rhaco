b882b103b770e02e91a163d880b8f9e4e210cae1
Matty Jones <mattyjones@boisestate.edu>
2018-04-05 14:50:17 -0600

    Started an atom mass table, we can add errors when an atom type is not defined here later

--- lynx/definitions.py
+++ lynx/definitions.py
@@ -3,3 +3,5 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PDB_LIBRARY = os.path.join(PROJECT_ROOT, 'compounds')
 FF_LIBRARY = os.path.join(PROJECT_ROOT, 'forcefields')
+# Atom masses obtained from NIST
+ATOM_MASSES = {'H': 1.00784, 'He': 4.00260, 'C': 12.00960, 'O': 15.99903}
