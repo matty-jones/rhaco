265fd755476c26bbce58b87904a3ed56b61bfceb
Matthew Jones <mattyjones@boisestate.edu>
2018-08-17 16:50:30 -0600

    Added a list of acceptable forcefield types to the definitions

--- rhaco/definitions.py
+++ rhaco/definitions.py
@@ -3,6 +3,7 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PDB_LIBRARY = os.path.join(PROJECT_ROOT, 'compounds')
 FF_LIBRARY = os.path.join(PROJECT_ROOT, 'forcefields')
+PERMITTED_FF_FORMATS = ['xml', 'fs.eam']
 # Atom masses obtained from NIST
 ATOM_MASSES = {'H': 1.00784, 'He': 4.00260, 'C': 12.00960, 'O': 15.99903,
                'Ni': 58.69344, 'Mn': 54.93804, 'Ga': 69.72310, 'Si': 28.08400}
