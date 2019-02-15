5fe5d8d7d16a24a0bfe8c40201df82a46f420467
Matthew Jones <mattyjones@boisestate.edu>
2019-02-15 16:50:46 -0700

    Separating out the EAM forcefield modification subroutines. Currently broken

--- rhaco/forcefields/create_forcefield.py
+++ rhaco/forcefields/create_forcefield.py
@@ -1,5 +1,6 @@
 import copy
 from rhaco.definitions import ATOM_MASSES
+from rhaco.definitions import ATOMIC_NUMBERS
 
 
 def update_EAM_forcefield(file_name, elements_list):
@@ -25,8 +26,17 @@ def update_EAM_forcefield(file_name, elements_list):
     # Complete header
     file_header = FF_lines[:5]
 
+
     # First element line: Atomic Number, Atomic Mass (AMU), Lattice Constant (Ang), Lattice Type (fcc, bcc, etc.)
     first_element_line = [FF_lines[5]]
+
+    # We can now pop the element that already exists from the elements_list so that
+    # we don't double count it
+    original_element_atomic = int(first_element_line[0].split()[0])
+    for element, atomic_number in ATOMIC_NUMBERS.items():
+        if atomic_number == original_element_atomic:
+            elements_list.remove(element)
+            break
 
     first_FF_line = FF_lines[6]
     numbers_per_line = len(first_FF_line.split())
@@ -43,9 +53,6 @@ def update_EAM_forcefield(file_name, elements_list):
     # Again there are nr values
     FF_ends_line = dens_func_fin_before + (nr // numbers_per_line)
     first_element_potentials = FF_lines[dens_func_fin_before:FF_ends_line]
-    print(first_element_emb_func[0], "...", first_element_emb_func[-1])
-    print(first_element_dens_func[0], "...", first_element_dens_func[-1])
-    print(first_element_potentials[0], "...", first_element_potentials[-1])
 
     # Now create the additional element lines
     new_element_lines = create_element_lines(
@@ -79,20 +86,22 @@ def update_EAM_forcefield(file_name, elements_list):
     # Add all the other interactions to forcefield
     for _ in range(int(((new_n_elements ** 2) + new_n_elements) / 2) - 1):
         potentials += blank_line * (nr // numbers_per_line)
-
     # For the embedding function and the density function
     embedding_function_lines = []
     density_function_lines = []
     for _ in range(new_n_elements):
         embedding_function_lines.append(blank_line * (nrho // numbers_per_line))
         density_function_lines.append(blank_line * (nrho // numbers_per_line))
-
     # Now reformat the EAM components
     print("Reformatting the EAM file components...")
     reformatted_EAM = reformat_EAM_components(
-        file_header, new_element_lines, embedding_function_lines,
-        density_function_lines,
+        file_header, new_element_lines,
+        [first_element_emb_func] + embedding_function_lines,
+        [first_element_dens_func] + density_function_lines,
+        potentials,
     )
+    print("Writing the new EAM file...")
+    write_new_EAM_file(file_name, reformatted_EAM, elements_list)
 
 
 def create_element_lines(first_element_line, elements_list):
@@ -104,70 +113,77 @@ def create_element_lines(first_element_line, elements_list):
     additional_element_lines = first_element_line
     for element in elements_list:
         additional_element_lines.append(
-            ["   ".join(
+            "   ".join(
                 [
-                    "13", "{:24.16E}".format(ATOM_MASSES[element]),
+                    "{:5d}".format(ATOMIC_NUMBERS[element]),
+                    "{:24.16E}".format(ATOM_MASSES[element]),
                     "{:24.16E}".format(0.0), "fcc\n"
                 ]
-            )]
+            )
         )
     return additional_element_lines
 
 
 def reformat_EAM_components(
-    file_header, element_lines, embed_func_lines, dens_func_lines,
+    file_header, element_lines, embed_func_lines, dens_func_lines, potentials,
     new_numbers_per_line=4,
 ):
     new_FF_lines = file_header
+    new_line_format = "".join(["{:20.12E}" * new_numbers_per_line, "\n"])
     for element_ID, element_line in enumerate(element_lines):
-        element_floats = [
-            list(map(float, embed_func_lines[element_ID][:-1].split())),
-            list(map(float, dens_func_lines[element_ID][:-1].split())),
+        print([value for sublist in [list(map(float, line.split())) for line in embed_func_lines[element_ID]] for value in sublist])
+        # Obtain a flat list of the floats for the embedding function
+        embed = [
+            value for sublist in
+            [list(map(float, line.split())) for line in embed_func_lines[element_ID]]
+            for value in sublist
         ]
-        
-        
-        ***THIS IS WHERE I AM UP TO ***
-        new_line_format = "".join(["{:20.12E}" * new_numbers_per_line, "\n"])
-        new_FF_lines += element_names[elementID]
+        # Obtain a flat list of the floats for the embedding function
+        dens = [
+            value for sublist in
+            [list(map(float, line.split())) for line in dens_func_lines[element_ID]]
+            for value in sublist
+        ]
+        element_floats = embed + dens
+        new_FF_lines += element_lines[element_ID]
         # Create a set of iterators of dynamic length to iterate through the number list
         # effectively creating a rolling `window' of new_numbers_per_line to format
-        iterators = [[iter(element_floats[i::new_numbers_per_line]) for i in range(new_numbers_per_line)]]
+        iterators = [
+            [
+                iter(element_floats[i::new_numbers_per_line])
+                for i in range(new_numbers_per_line)
+            ]
+        ]
         line_of_floats = [list(zip(*iterator)) for iterator in iterators][0]
         for values in line_of_floats:
             new_FF_lines += new_line_format.format(*values)
+    # Finally, add on the potentials to the end
+    print(potentials[:10])
+    potn = [
+        value for sublist in
+        [list(map(float, line.split())) for line in potentials]
+        for value in sublist
+    ]
+    # Split potn_vals into equal chunks of new_numbers_per_line
+    potn_vals = [
+        potn[i: i + new_numbers_per_line]
+        for i in range(0, len(potn), new_numbers_per_line)
+    ]
+    for line in potn_vals:
+        new_FF_lines += new_line_format.format(*line)
+    return new_FF_lines
 
 
+def write_new_EAM_file(original_file_name, reformatted_EAM, elements_list):
+    print(elements_list)
+    print()
+    file_name = original_file_name.replace(
+        ".eam", "".join(["_".join(["_inc"] + elements_list), ".eam"])
+    )
+    with open(file_name, "w+") as new_FF_file:
+        new_FF_file.writelines(reformatted_EAM)
+    print("EAM file written to", file_name)
 
 
 if __name__ == "__main__":
     update_EAM_forcefield("Ag_Zhou04.eam.alloy", ["Ag", "Al", "O"])
-
-
-
-
-    new_FF_lines = file_header
-    new_numbers_per_line = 4
-    element_names = {0: first_element_line,
-                     1: second_element_line,
-                     2: third_element_line}
-    # Based on the *.eam.alloy format, here is the correct ordering:
-    element_properties = {0: [first_element_emb_func, first_element_dens_func],
-                          1: [second_element_emb_func, second_element_dens_func],
-                          2: [third_element_emb_func, third_element_dens_func, potentials],
-                         }
-    for elementID in range(3):
-        element_floats = []
-        for element_props in element_properties[elementID]:
-            for line in element_props:
-                element_floats += list(map(float, line[:-1].split()))
-
-    # print("Concatenating the eam file components...")
-    # # Now we have all the components needed to write the file:
-    # new_FF_lines = file_header + first_element_line + first_element_emb_func + first_element_dens_func + first_element_potentials + second_element_line + second_element_emb_func + second_element_dens_func + second_element_potentials + third_element_line + third_element_emb_func + third_element_dens_func + third_element_potentials
-
-    print("Writing the new eam file...")
-    #file_name = "corundum.eam.fs"
-    file_name = "corundum_Zhou04.eam.alloy"
-    with open(file_name, "w+") as new_FF_file:
-        new_FF_file.writelines(new_FF_lines)
-    print("EAM file written to", file_name)
