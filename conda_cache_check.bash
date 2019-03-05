152bf72d303df889373e832797e1b9a29cae1da8
Matthew Jones <mattyjones@boisestate.edu>
2019-03-05 16:44:24 -0700

    Added the shippable and codecov integration for rhaco

---
+++ conda_cache_check.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ -d /opt/conda/envs/rhaco ] && [ $BRANCH != 'master' ]; then
+	echo "Using Cache";
+	source activate rhaco
+else
+	echo "Rebuilding Conda Env";
+    rm -rf /opt/conda/envs/rhaco
+	conda env create -f environment.yml;
+        source activate rhaco
+fi
