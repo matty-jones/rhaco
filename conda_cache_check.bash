316d657e90dd9304769b69ff79e73d637d8c40bb
Matthew Jones <mattyjones@boisestate.edu>
2019-03-06 16:07:00 -0700

    Dropping hoomd_dybond back in for shippable but leaving it out of the environment.yml

--- conda_cache_check.bash
+++ conda_cache_check.bash
@@ -3,10 +3,12 @@
 if [ -d /opt/conda/envs/rhaco ] && [ $BRANCH != 'master' ]; then
     rm -rf /opt/conda/envs/rhaco
 	conda env create -f environment.yml;
+    conda install -c cmelab hoomd_dybond
 	source activate rhaco
 else
 	echo "Rebuilding Conda Env";
     rm -rf /opt/conda/envs/rhaco
 	conda env create -f environment.yml;
-        source activate rhaco
+    conda install -c cmelab hoomd_dybond
+    source activate rhaco
 fi
