72879a64127192d355f414eccec5ddb5f78700bd
Matthew Jones <mattyjones@boisestate.edu>
2019-03-06 17:10:42 -0700

    Returned the conda cache check back to what it needs to be

--- conda_cache_check.bash
+++ conda_cache_check.bash
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 if [ -d /opt/conda/envs/rhaco ] && [ $BRANCH != 'master' ]; then
-    rm -rf /opt/conda/envs/rhaco;
-    conda create --name rhaco --clone base;
-    source activate rhaco;
-    pip install pytest-cov PyYAML codecov numba numpy-quaternion;
-    pip install -e .;
+	echo "Using Cache";
+	source activate rhaco;
 else
 	echo "Rebuilding Conda Env";
     rm -rf /opt/conda/envs/rhaco;
