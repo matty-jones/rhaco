a635144dec881ce2ac0b6ee7e5e48dbd3c496263
Matthew Jones <mattyjones@boisestate.edu>
2019-03-06 17:05:08 -0700

    This updated environment for shippable now works locally using Python 3.6.8 (same as planckton)

--- conda_cache_check.bash
+++ conda_cache_check.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 if [ -d /opt/conda/envs/rhaco ] && [ $BRANCH != 'master' ]; then
-    rm -rf /opt/conda/envs/rhaco
-	conda env create -f environment.yml;
-	source activate rhaco
+    rm -rf /opt/conda/envs/rhaco;
+    conda create --name rhaco --clone base;
+    source activate rhaco;
+    pip install pytest-cov PyYAML codecov numba numpy-quaternion;
+    pip install -e .;
 else
 	echo "Rebuilding Conda Env";
-    rm -rf /opt/conda/envs/rhaco
-	conda env create -f environment.yml;
-    source activate rhaco
+    rm -rf /opt/conda/envs/rhaco;
+    conda create --name rhaco --clone base;
+    source activate rhaco;
+    pip install pytest-cov PyYAML codecov numba numpy-quaternion;
+    pip install -e .;
 fi
