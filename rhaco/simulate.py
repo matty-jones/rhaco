35823918350abff4bb79e6509a7052bac9be6b02
Matthew Jones <mattyjones@boisestate.edu>
2019-03-11 16:34:03 -0600

    Added omit_lj back in now that we cannot guarantee that we can treat the required crystal and reactant components as the same rigid bodies

--- rhaco/simulate.py
+++ rhaco/simulate.py
@@ -20,9 +20,29 @@ AMU_TO_KG = 1.6605E-27
 ANG_TO_M = 1E-10
 
 
+def parse_interactions(omit_string):
+    omit_list = []
+    if (omit_string[0] == "[") and (omit_string[-1] == "]"):
+        omit_string = omit_string[1:-1]
+    if "," in omit_string:
+        omit_string = "".join(omit_string.split(" "))
+        omit_string = omit_string.split(",")
+    else:
+        omit_string = omit_string.split()
+    for interaction in omit_string:
+        if (interaction[0] == "'") and (interaction[-1] == "'"):
+            omit_list.append(interaction[1:-1])
+        elif (interaction[0] == '"') and (interaction[-1] == '"'):
+            omit_list.append(interaction[1:-1])
+        else:
+            omit_list.append(interaction)
+    return omit_list
+
+
 def set_coeffs(
     file_name,
     system,
+    omit_lj,
     distance_scaling,
     energy_scaling,
     nl_type,
@@ -59,9 +79,12 @@ def set_coeffs(
         for type1 in coeffs_dict["pair_coeffs"]:
             for type2 in coeffs_dict["pair_coeffs"]:
                 if (
-                    not generate_arguments["integrate_crystal"]
-                    and type1[0][:2] == "X_"
-                    and type2[0][:2] == "X_"
+                    (
+                        not generate_arguments["integrate_crystal"]
+                        and type1[0][:2] == "X_"
+                        and type2[0][:2] == "X_"
+                    )
+                    or ("-".join([type1[0], type2[0]]) in omit_lj)
                 ):
                     lj_r_cut = 0.0
                 else:
@@ -638,6 +661,13 @@ def main():
         help="""The thermostat coupling to use when running
                         the NVT MD simulation.\n""",
     )
+    parser.add_argument('-o', '--omit_lj',
+                        type=parse_interactions,
+                        default=[],
+                        required=False,
+                        help='''A list of lj interactions to omit from the
+                        rhaco-generated input hoomdxml (useful when using EAM).\n
+                        If unspecified, all interactions are considered''')
     parser.add_argument(
         "-e",
         "--energy_scale_unit",
@@ -748,6 +778,7 @@ def main():
         system, log_quantities = set_coeffs(
             file_name,
             system,
+            args.omit_lj,
             args.distance_scale_unit,
             args.energy_scale_unit,
             args.nl_type,
