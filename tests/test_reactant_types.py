2dfb4732705bfb6c558ed99e93d334ada0e399c9
Matthew Jones <mattyjones@boisestate.edu>
2019-03-11 17:29:47 -0600

    Added omit-lj into the testing stack for use with the combined forcefields

--- tests/test_reactant_types.py
+++ tests/test_reactant_types.py
@@ -17,7 +17,7 @@ import subprocess as sp
         #{"--dimensions": "7x3x1", "--z_reactor_size": "15.0", "--crystal_separation": "9.0", "--reactant_composition": "{'C2H6': 1.0, 'perylene': 1.0, 'Ag_5nm': 'pos'}", "--reactant_density": "0.05", "--reactant_rigid": "['perylene']", "--reactant_position": "[[-3.0, 0.0, 3.5], [3.0, 0.0, 3.5]]", "--forcefield": "FF_opls_uff_aromatic"},
         #{"--template": "Ag_surface.pdb", "--dimensions": "10x10x1", "--z_reactor_size": "15.0", "--crystal_separation": "9.0", "--reactant_composition": "{'Ag_5nm': 'pos'}", "--reactant_position": "[[0.0, 0.0, 4.5]]", "--forcefield": "Ag_Zhou04.eam.alloy", "--crystal_x": "0.781", "--crystal_y": "0.781", "--crystal_z": "0.644"},
         #{"--template": "corundum.pdb", "--dimensions": "15x15x1", "--z_reactor_size": "15.0", "--crystal_separation": "15.0", "--reactant_composition": "{'Ag_5nm': 'pos'}", "--reactant_position": "[[0.0, 0.0, 4.5]]", "--forcefield": "Ag_Zhou04.eam.alloy", "--crystal_x": "0.4759", "--crystal_y": "0.4759", "--crystal_z": "1.299"},
-        {"--template": "corundum.pdb", "--dimensions": "15x15x1", "--z_reactor_size": "15.0", "--crystal_separation": "15.0", "--reactant_composition": "{'Ag_5nm': 'pos', 'C2H6': 1}", "--reactant_position": "[[0.0, 0.0, 4.5]]", "--reactant_density": "0.05", "--forcefield": "['Ag_Zhou04.eam.alloy', 'FF_opls_elliott.xml']", "--crystal_x": "0.4759", "--crystal_y": "0.4759", "--crystal_z": "1.299"},
+        {"--template": "corundum.pdb", "--dimensions": "15x15x1", "--z_reactor_size": "15.0", "--crystal_separation": "15.0", "--reactant_composition": "{'Ag_5nm': 'pos', 'C2H6': 1}", "--reactant_position": "[[0.0, 0.0, 4.5]]", "--reactant_density": "0.05", "--forcefield": "['Ag_Zhou04.eam.alloy', 'FF_opls_elliott.xml']", "--crystal_x": "0.4759", "--crystal_y": "0.4759", "--crystal_z": "1.299", "--omit_lj": "['Ag-Ag']"},
     ],
     # ids=[
     #     "M1_flex_num", "M1_flex_dens", "M1_rig", "M1_pos", "M1_flex_rig",
@@ -42,13 +42,19 @@ def create_morph(request):
     print(shell_output)
     output_file_name_line = shell_output[0].decode("utf-8").split("\n")[-3]
     output_file = output_file_name_line.split("XML file written to ")[1][:-1]
-    return os.path.abspath(output_file)
+    simulate_arguments = None
+    print("\n\n\n --== FLAGS DICT ==--")
+    print(flags_dict)
+    if "--omit_lj" in flags_dict.keys():
+        print(flags_dict["--omit_lj"])
+        simulate_arguments = {"--omit_lj": flags_dict["--omit_lj"]}
+    return os.path.abspath(output_file), simulate_arguments
 
 
 class TestCreateMorphology():
     @pytest.mark.order1
     def test_check_xml_created(self, create_morph):
-        (directory, file_name) = os.path.split(create_morph)
+        (directory, file_name) = os.path.split(create_morph[0])
         files = os.listdir(directory)
         assert file_name in files, "".join(
             [
@@ -62,14 +68,18 @@ class TestCreateMorphology():
 
     @pytest.mark.order2
     def test_check_run_hoomd(self, create_morph):
-        run_hoomd_output = sp.Popen(
-            ["rhaco-run-hoomd", "-r", "10", create_morph], stdout=sp.PIPE
-        ).communicate()
+        run_hoomd_cmd = ["rhaco-run-hoomd", "-r", "10"]
+        if create_morph[1] is not None:
+            for key, val in create_morph[1].items():
+                run_hoomd_cmd.append(key)
+                run_hoomd_cmd.append(val)
+        run_hoomd_cmd.append(create_morph[0])
+        run_hoomd_output = sp.Popen(run_hoomd_cmd, stdout=sp.PIPE).communicate()
         print(run_hoomd_output[0].decode("utf-8"))
-        (directory, file_name) = os.path.split(create_morph)
+        (directory, file_name) = os.path.split(create_morph[0])
         morphology_name = os.path.splitext(file_name)[0]
         hoomd_run_files = [
-            create_morph,
+            create_morph[0],
             "".join([os.path.abspath(morphology_name), ".log"]),
             "".join([os.path.abspath(morphology_name), "_final.gsd"]),
             "".join([os.path.abspath(morphology_name), "_traj.gsd"]),
